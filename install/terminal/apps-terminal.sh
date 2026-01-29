da8af67aa259ee351197800eb7f07046fddcdbac
Szamski <maciek.szamowski@gmail.com>
2026-01-29 16:37:14 +0100

    Install tldr via pipx

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt install -y fzf ripgrep bat eza zoxide plocate apache2-utils fd-find tldr
+sudo apt install -y fzf ripgrep bat eza zoxide plocate apache2-utils fd-find
+
+if command -v pipx >/dev/null 2>&1; then
+  if ! command -v tldr >/dev/null 2>&1; then
+    pipx install tldr
+  fi
+fi
